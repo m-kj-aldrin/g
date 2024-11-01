b38854395d00a547a1c442de354e340921c628ce
m-kj-aldrin <mkj.aldrin@icloud.com>
2024-11-01 13:52:18 +0100

    smaler api surface

--- test/scene.js
+++ test/scene.js
@@ -1,89 +1,14 @@
-import { Mat2, Mat3, Mat4, Vec2, Vec3 } from "../main.js";
+import { Mat3, Mat4, Vec2, Vec3 } from "../main.js";
 
-// let v2_0 = new Vec2(1, 1);
-// let m2_scale_0 = Mat2.fromScale(new Vec2(10, 100));
-// let m3_0 = new Mat3(1, 0, 0, 0, 1, 0, 10, 100, 0);
+let m3_00 = Mat3.fromScaling(new Vec2(10, 100));
+let m3_01 = Mat3.fromTranslation(new Vec2(2, 1));
 
-// v2_0.multiply(m2_scale_0).multiply(m3_0);
+let m3_model_matrix = Mat3.multiply(m3_00, m3_01);
 
-// console.log(v2_0);
+let v2 = new Vec2(0, 0);
 
-// let v3_0 = new Vec3(1, 1, 0);
-// let m3_1 = new Mat3(1, 0, 0, 0, 10, 0, 0, 0, 1);
-// let m4_0 = new Mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 2, 3, 0);
+let v2_transformed = v2.clone().multiply(m3_model_matrix);
 
-// v3_0.multiply(m4_0).multiply(m3_1);
+console.log(v2_transformed.toString());
 
-// console.log(v3_0);
-
-// let v2_0 = new Vec2(1, 0);
-// let m2_rot_0 = Mat2.fromRotation(Math.PI / 2);
-
-// v2_0.multiply(m2_rot_0);
-
-// console.log(v2_0);
-
-// let v3_0 = new Vec3(1, 0, 0);
-// let m3_rot_0 = Mat3.fromAxisRotation(new Vec3(0, 0, 1), Math.PI / 4);
-// let m3_scale_0 = Mat3.fromScale(new Vec3(10, 100, 1));
-
-// let m3_modelMatrix_0 = new Mat3().multiply(m3_scale_0).multiply(m3_rot_0);
-
-// v3_0.multiply(m3_modelMatrix_0);
-
-// console.log(v3_0);
-
-// let v2_0 = new Vec2(1, 1);
-// let m2_scale_0 = Mat2.fromScale(new Vec2(10, 100));
-// let m3_translate_0 = Mat3.fromTranslate(new Vec2(1, 2));
-
-// let m3_modelMatrix_1 = Mat3.multiply(m3_translate_0, m2_scale_0);
-
-// v2_0.multiply(m3_modelMatrix_1);
-
-// console.log(v2_0);
-
-// // console.log(m3_modelMatrix_1.elements);
-
-// let m2_00 = Mat2.fromScaling(new Vec2(50, 1));
-// let m2_01 = Mat2.fromRotation(Math.PI / 4);
-// let m3_00 = Mat3.fromTranslation(new Vec2(300, -50));
-
-// let m2_model_matrix = Mat3.multiply(m3_00, m2_01, m2_00);
-
-// console.log(m2_00.toString());
-
-// console.log(m2_model_matrix.toString());
-
-// let v2_00 = new Vec2(1, 0);
-// let v2_00_transformed = v2_00.clone().multiply(m2_model_matrix);
-
-// console.log(v2_00_transformed.toString());
-
-// let v3_00 = new Vec3(1,2,3).multiply(m3_00)
-
-// console.log(v3_00.toString());
-
-// let m3_rot_00 = Mat3.fromAxisRotation(new Vec3(0, 0, 1), Math.PI / 2);
-// let m4_translate_00 = Mat4.fromTranslation(new Vec3(10, 100, 100));
-
-// let m4_model_matrix_00 = Mat4.multiply(m4_translate_00, m3_rot_00);
-
-// console.log(m4_model_matrix_00.toString());
-
-// let v3_000 = new Vec3(1, 0, 0);
-
-// console.log(v3_000.toString());
-
-// v3_000.multiply(m4_model_matrix_00);
-
-// console.log(v3_000.toString());
-
-let m4_rot_00 = Mat4.fromAxisRotation(new Vec3(0, 0, 1), Math.PI / 4);
-let m4_scaling_00 = Mat4.fromScaling(new Vec3(1, 10, 1));
-
-let m4_model_matrix_01 = Mat4.multiply(m4_scaling_00, m4_rot_00);
-
-let v3_0000 = new Vec3(1, 0, 0).multiply(m4_model_matrix_01);
-
-console.log(v3_0000.toString());
+console.log(v2_transformed.dot(v2_transformed));
--- vec-mat/index.js
+++ vec-mat/index.js
@@ -18,26 +18,99 @@ class Vec2 {
     }
 
     /**
-     * Multiplies the vector either by a vector component wise or with 2x2 matrix as an direction vector or 3x3 matrix as a position vector
-     * @param {Vec2 | Mat2 | Mat3} vectorOrMatrix
+     * Adds another Vec2 to this vector component-wise.
+     * @param {Vec2} vector
+     * @returns {Vec2} This vector after addition.
+     */
+    add(vector) {
+        if (!(vector instanceof Vec2)) {
+            throw new TypeError("Argument must be an instance of Vec2");
+        }
+        this.#x += vector.#x;
+        this.#y += vector.#y;
+        return this;
+    }
+
+    /**
+     * Subtracts another Vec2 from this vector component-wise.
+     * @param {Vec2} vector
+     * @returns {Vec2} This vector after subtraction.
+     */
+    subtract(vector) {
+        if (!(vector instanceof Vec2)) {
+            throw new TypeError("Argument must be an instance of Vec2");
+        }
+        this.#x -= vector.#x;
+        this.#y -= vector.#y;
+        return this;
+    }
+
+    /**
+     * Computes the dot product with another Vec2.
+     * @param {Vec2} vector
+     * @returns {number} The dot product.
+     */
+    dot(vector) {
+        if (!(vector instanceof Vec2)) {
+            throw new TypeError("Argument must be an instance of Vec2");
+        }
+        return this.#x * vector.#x + this.#y * vector.#y;
+    }
+
+    /**
+     * Multiplies this vector by a scalar.
+     * @param {number} scalar
+     * @returns {Vec2} This vector after multiplication.
+     */
+    scalarMultiply(scalar) {
+        if (typeof scalar !== "number") {
+            throw new TypeError("Scalar must be a number");
+        }
+        this.#x *= scalar;
+        this.#y *= scalar;
+        return this;
+    }
+
+    /**
+     * Divides this vector by a scalar.
+     * @param {number} scalar
+     * @returns {Vec2} This vector after division.
+     */
+    scalarDivide(scalar) {
+        if (typeof scalar !== "number") {
+            throw new TypeError("Scalar must be a number");
+        }
+        if (scalar === 0) {
+            throw new Error("Division by zero");
+        }
+        this.#x /= scalar;
+        this.#y /= scalar;
+        return this;
+    }
+
+    /**
+     * Computes the Euclidean distance to another Vec2.
+     * @param {Vec2} vector
+     * @returns {number} The distance between the two vectors.
+     */
+    distanceTo(vector) {
+        if (!(vector instanceof Vec2)) {
+            throw new TypeError("Argument must be an instance of Vec2");
+        }
+        const dx = this.#x - vector.#x;
+        const dy = this.#y - vector.#y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    /**
+     * Multiplies the vector either by a vector component-wise or with a matrix.
+     * @param {Vec2 | Mat3} vectorOrMatrix
+     * @returns {Vec2} This vector after multiplication.
      */
     multiply(vectorOrMatrix) {
         if (vectorOrMatrix instanceof Vec2) {
             this.#x *= vectorOrMatrix.#x;
             this.#y *= vectorOrMatrix.#y;
-
-            return this;
-        }
-
-        if (vectorOrMatrix instanceof Mat2) {
-            let e = vectorOrMatrix.elements;
-
-            let _x = this.#x * e[0] + this.#y * e[2];
-            let _y = this.#x * e[1] + this.#y * e[3];
-
-            this.#x = _x;
-            this.#y = _y;
-
             return this;
         }
 
@@ -54,9 +127,7 @@ class Vec2 {
             return this;
         }
 
-        throw new TypeError(
-            "vectorOrMatrix needs to be of type Vec2 | Mat2 | Mat3"
-        );
+        throw new TypeError("vectorOrMatrix needs to be of type Vec2 | Mat3");
     }
 
     get length() {
@@ -65,9 +136,11 @@ class Vec2 {
 
     normalize() {
         let length = this.length;
+        if (length === 0) {
+            throw new Error("Cannot normalize a zero-length vector");
+        }
         this.#x /= length;
         this.#y /= length;
-
         return this;
     }
 
@@ -106,29 +179,125 @@ class Vec3 {
     }
 
     /**
-     * Multiplies the vector either by a vector component wise or with 3x3 matrix as an direction vector or 4x4 matrix as a position vector
-     * @param {Vec3 | Mat3 | Mat4} vectorOrMatrix
+     * Adds another Vec3 to this vector component-wise.
+     * @param {Vec3} vector
+     * @returns {Vec3} This vector after addition.
+     */
+    add(vector) {
+        if (!(vector instanceof Vec3)) {
+            throw new TypeError("Argument must be an instance of Vec3");
+        }
+        this.#x += vector.#x;
+        this.#y += vector.#y;
+        this.#z += vector.#z;
+        return this;
+    }
+
+    /**
+     * Subtracts another Vec3 from this vector component-wise.
+     * @param {Vec3} vector
+     * @returns {Vec3} This vector after subtraction.
+     */
+    subtract(vector) {
+        if (!(vector instanceof Vec3)) {
+            throw new TypeError("Argument must be an instance of Vec3");
+        }
+        this.#x -= vector.#x;
+        this.#y -= vector.#y;
+        this.#z -= vector.#z;
+        return this;
+    }
+
+    /**
+     * Computes the dot product with another Vec3.
+     * @param {Vec3} vector
+     * @returns {number} The dot product.
+     */
+    dot(vector) {
+        if (!(vector instanceof Vec3)) {
+            throw new TypeError("Argument must be an instance of Vec3");
+        }
+        return this.#x * vector.#x + this.#y * vector.#y + this.#z * vector.#z;
+    }
+
+    /**
+     * Multiplies this vector by a scalar.
+     * @param {number} scalar
+     * @returns {Vec3} This vector after multiplication.
+     */
+    scalarMultiply(scalar) {
+        if (typeof scalar !== "number") {
+            throw new TypeError("Scalar must be a number");
+        }
+        this.#x *= scalar;
+        this.#y *= scalar;
+        this.#z *= scalar;
+        return this;
+    }
+
+    /**
+     * Divides this vector by a scalar.
+     * @param {number} scalar
+     * @returns {Vec3} This vector after division.
+     */
+    scalarDivide(scalar) {
+        if (typeof scalar !== "number") {
+            throw new TypeError("Scalar must be a number");
+        }
+        if (scalar === 0) {
+            throw new Error("Division by zero");
+        }
+        this.#x /= scalar;
+        this.#y /= scalar;
+        this.#z /= scalar;
+        return this;
+    }
+
+    /**
+     * Computes the cross product with another Vec3 and updates this vector.
+     * @param {Vec3} vector
+     * @returns {Vec3} This vector after the cross product.
+     */
+    cross(vector) {
+        if (!(vector instanceof Vec3)) {
+            throw new TypeError("Argument must be an instance of Vec3");
+        }
+        const crossX = this.#y * vector.#z - this.#z * vector.#y;
+        const crossY = this.#z * vector.#x - this.#x * vector.#z;
+        const crossZ = this.#x * vector.#y - this.#y * vector.#x;
+
+        this.#x = crossX;
+        this.#y = crossY;
+        this.#z = crossZ;
+
+        return this;
+    }
+
+    /**
+     * Computes the Euclidean distance to another Vec3.
+     * @param {Vec3} vector
+     * @returns {number} The distance between the two vectors.
+     */
+    distanceTo(vector) {
+        if (!(vector instanceof Vec3)) {
+            throw new TypeError("Argument must be an instance of Vec3");
+        }
+        const dx = this.#x - vector.#x;
+        const dy = this.#y - vector.#y;
+        const dz = this.#z - vector.#z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    /**
+     * Multiplies the vector either by a vector component-wise or with a matrix.
+     * @param {Vec3 | Mat4} vectorOrMatrix
+     * @returns {Vec3} This vector after multiplication.
      */
     multiply(vectorOrMatrix) {
         if (vectorOrMatrix instanceof Vec3) {
             this.#x *= vectorOrMatrix.#x;
             this.#y *= vectorOrMatrix.#y;
             this.#z *= vectorOrMatrix.#z;
-
-            return this;
-        }
-
-        if (vectorOrMatrix instanceof Mat3) {
-            let e = vectorOrMatrix.elements;
-
-            let _x = this.#x * e[0] + this.#y * e[3] + this.#z * e[6];
-            let _y = this.#x * e[1] + this.#y * e[4] + this.#z * e[7];
-            let _z = this.#x * e[2] + this.#y * e[5] + this.#z * e[8];
-
-            this.#x = _x;
-            this.#y = _y;
-            this.#z = _z;
-
             return this;
         }
 
@@ -153,9 +322,7 @@ class Vec3 {
             return this;
         }
 
-        throw new TypeError(
-            "vectorOrMatrix needs to be of type Vec3 | Mat3 | Mat4"
-        );
+        throw new TypeError("vectorOrMatrix needs to be of type Vec3 | Mat4");
     }
 
     get length() {
@@ -164,6 +331,9 @@ class Vec3 {
 
     normalize() {
         let length = this.length;
+        if (length === 0) {
+            throw new Error("Cannot normalize a zero-length vector");
+        }
         this.#x /= length;
         this.#y /= length;
         this.#z /= length;
@@ -181,84 +351,16 @@ class Vec3 {
     }
 }
 
-class Mat2 {
-    /**
-     * Matrix elements in column-major
-     */
-    #elements = new Float32Array([1, 0, 0, 1]);
-
-    constructor(...elements) {
-        if (elements.length === 4) {
-            this.#elements.set(elements);
-        }
-    }
-
-    get elements() {
-        return this.#elements;
-    }
-
-    /**
-     * @param {Mat2} matrix
-     */
-    multiply(matrix) {
-        let a = this.#elements;
-        let b = matrix.#elements;
-
-        let result = new Float32Array(4);
-
-        for (let col = 0; col < 2; col++) {
-            for (let row = 0; row < 2; row++) {
-                result[col * 2 + row] =
-                    a[0 * 2 + row] * b[col * 2 + 0] +
-                    a[1 * 2 + row] * b[col * 2 + 1];
-            }
-        }
-
-        this.#elements.set(result);
-
-        return this;
-    }
-
-    clone() {
-        return new Mat2(this.#elements);
-    }
-
-    toString() {
-        return matrixToString(this.#elements, 2, 2, 2, "Mat2");
-    }
-
-    /**
-     * @param {...(Mat2)} matrix
-     */
-    static multiply(...matrix) {
-        return matrix.reduce((r, m) => r.multiply(m), new Mat2());
-    }
-
-    /**
-     * @param {Vec2} vector
-     */
-    static fromScaling(vector) {
-        let { x, y } = vector;
-        return new Mat2(x, 0, 0, y);
-    }
-
-    /**
-     * @param {number} angle
-     */
-    static fromRotation(angle) {
-        let cos = Math.cos(angle);
-        let sin = Math.sin(angle);
-
-        return new Mat2(cos, sin, -sin, cos);
-    }
-}
-
 class Mat3 {
     /**
      * Matrix elements in column-major
      */
     #elements = new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1]);
 
+    /**
+     *
+     * @param  {...number} elements
+     */
     constructor(...elements) {
         if (elements.length === 9) {
             this.#elements.set(elements);
@@ -271,30 +373,28 @@ class Mat3 {
 
     /**
      *
-     * @param {Mat2 | Mat3 } matrix
+     * @param { Mat3 } matrix
      */
     multiply(matrix) {
         let a = this.#elements;
-        let b = new Float32Array(9);
+        let b = matrix.#elements;
 
-        let other = matrix.elements;
+        let result = new Float32Array(9);
 
-        if (matrix instanceof Mat2) {
-            b.set([...other.slice(0, 2), 0, ...other.slice(2, 4), 0, 0, 0, 1]);
-        } else if (matrix instanceof Mat3) {
-            b.set(matrix.elements);
-        }
+        // First Column
+        result[0] = a[0] * b[0] + a[3] * b[1] + a[6] * b[2];
+        result[1] = a[1] * b[0] + a[4] * b[1] + a[7] * b[2];
+        result[2] = a[2] * b[0] + a[5] * b[1] + a[8] * b[2];
 
-        let result = new Float32Array({ length: 9 });
+        // Second Column
+        result[3] = a[0] * b[3] + a[3] * b[4] + a[6] * b[5];
+        result[4] = a[1] * b[3] + a[4] * b[4] + a[7] * b[5];
+        result[5] = a[2] * b[3] + a[5] * b[4] + a[8] * b[5];
 
-        for (let col = 0; col < 3; col++) {
-            for (let row = 0; row < 3; row++) {
-                result[col * 3 + row] =
-                    a[0 * 3 + row] * b[col * 3 + 0] +
-                    a[1 * 3 + row] * b[col * 3 + 1] +
-                    a[2 * 3 + row] * b[col * 3 + 2];
-            }
-        }
+        // Third Column
+        result[6] = a[0] * b[6] + a[3] * b[7] + a[6] * b[8];
+        result[7] = a[1] * b[6] + a[4] * b[7] + a[7] * b[8];
+        result[8] = a[2] * b[6] + a[5] * b[7] + a[8] * b[8];
 
         this.#elements.set(result);
 
@@ -311,53 +411,29 @@ class Mat3 {
 
     /**
      *
-     * @param  {...(Mat2|Mat3|Mat4)} matrix
+     * @param  {...Mat3} matrix
      */
     static multiply(...matrix) {
-        return /**@type {Mat3}*/ (
-            matrix.reduce(
-                /**@param {Mat3} r*/ (r, m) => r.multiply(m),
-                new Mat3()
-            )
-        );
+        return matrix.reduce((r, m) => r.multiply(m), new Mat3());
     }
 
     /**
      *
-     * @param {Vec3} vector
+     * @param {Vec2} vector
      */
     static fromScaling(vector) {
-        let { x, y, z } = vector;
-        return new Mat3(x, 0, 0, 0, y, 0, 0, 0, z);
+        let { x, y } = vector;
+        return new Mat3(x, 0, 0, 0, y, 0, 0, 0, 1);
     }
 
     /**
      *
-     * @param {Vec3} axis
      * @param {number} angle
      */
-    static fromAxisRotation(axis, angle) {
-        let v = axis.clone().normalize();
-
-        let { x, y, z } = v;
-
-        let c = Math.cos(angle);
-        let s = Math.sin(angle);
-        let t = 1 - c;
-
-        let R11 = t * x * x + c;
-        let R12 = t * x * y - s * z;
-        let R13 = t * x * z + s * y;
-
-        let R21 = t * x * y + s * z;
-        let R22 = t * y * y + c;
-        let R23 = t * y * z - s * x;
-
-        let R31 = t * x * z - s * y;
-        let R32 = t * y * z + s * x;
-        let R33 = t * z * z + c;
-
-        return new Mat3(R11, R21, R31, R12, R22, R32, R13, R23, R33);
+    static fromRotation(angle) {
+        let cos = Math.cos(angle);
+        let sin = Math.sin(angle);
+        return new Mat3(cos, sin, 0 - sin, cos, 0, 0, 0, 1);
     }
 
     /**
@@ -388,43 +464,39 @@ class Mat4 {
     }
 
     /**
-     *
-     * @param {Mat3 | Mat4} matrix
+     * @param {Mat4} matrix
      */
     multiply(matrix) {
         let a = this.#elements;
-        let b = new Float32Array(16);
-
-        let other = matrix.elements;
-
-        if (matrix instanceof Mat3) {
-            b.set([
-                ...other.slice(0, 3),
-                0,
-                ...other.slice(3, 6),
-                0,
-                ...other.slice(6, 9),
-                0,
-                0,
-                0,
-                0,
-                1,
-            ]);
-        } else if (matrix instanceof Mat4) {
-            b.set(matrix.elements);
-        }
+        let b = matrix.#elements;
 
         let result = new Float32Array(16);
 
-        for (let col = 0; col < 4; col++) {
-            for (let row = 0; row < 4; row++) {
-                result[col * 4 + row] =
-                    a[0 * 4 + row] * b[col * 4 + 0] +
-                    a[1 * 4 + row] * b[col * 4 + 1] +
-                    a[2 * 4 + row] * b[col * 4 + 2] +
-                    a[3 * 4 + row] * b[col * 4 + 3];
-            }
-        }
+        // Column 0
+        result[0] = a[0] * b[0] + a[4] * b[1] + a[8] * b[2] + a[12] * b[3];
+        result[1] = a[1] * b[0] + a[5] * b[1] + a[9] * b[2] + a[13] * b[3];
+        result[2] = a[2] * b[0] + a[6] * b[1] + a[10] * b[2] + a[14] * b[3];
+        result[3] = a[3] * b[0] + a[7] * b[1] + a[11] * b[2] + a[15] * b[3];
+
+        // Column 1
+        result[4] = a[0] * b[4] + a[4] * b[5] + a[8] * b[6] + a[12] * b[7];
+        result[5] = a[1] * b[4] + a[5] * b[5] + a[9] * b[6] + a[13] * b[7];
+        result[6] = a[2] * b[4] + a[6] * b[5] + a[10] * b[6] + a[14] * b[7];
+        result[7] = a[3] * b[4] + a[7] * b[5] + a[11] * b[6] + a[15] * b[7];
+
+        // Column 2
+        result[8] = a[0] * b[8] + a[4] * b[9] + a[8] * b[10] + a[12] * b[11];
+        result[9] = a[1] * b[8] + a[5] * b[9] + a[9] * b[10] + a[13] * b[11];
+        result[10] = a[2] * b[8] + a[6] * b[9] + a[10] * b[10] + a[14] * b[11];
+        result[11] = a[3] * b[8] + a[7] * b[9] + a[11] * b[10] + a[15] * b[11];
+
+        // Column 3
+        result[12] = a[0] * b[12] + a[4] * b[13] + a[8] * b[14] + a[12] * b[15];
+        result[13] = a[1] * b[12] + a[5] * b[13] + a[9] * b[14] + a[13] * b[15];
+        result[14] =
+            a[2] * b[12] + a[6] * b[13] + a[10] * b[14] + a[14] * b[15];
+        result[15] =
+            a[3] * b[12] + a[7] * b[13] + a[11] * b[14] + a[15] * b[15];
 
         this.#elements.set(result);
 
@@ -441,7 +513,7 @@ class Mat4 {
 
     /**
      *
-     * @param {...(Mat3|Mat4)} matrix
+     * @param {...Mat4} matrix
      */
     static multiply(...matrix) {
         return matrix.reduce((r, m) => r.multiply(m), new Mat4());
@@ -510,4 +582,4 @@ class Mat4 {
     }
 }
 
-export { Vec2, Vec3, Mat2, Mat3, Mat4 };
+export { Vec2, Vec3, Mat3, Mat4 };
